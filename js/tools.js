860ff8a92749158c7479d9fe9dec69476019e78e
Ariadna Frias <ariadna.812@gmail.com>
2023-01-28 11:44:42 +0100

    Testing first function

--- js/tools.js
+++ js/tools.js
@@ -1,1 +1,17 @@
-export const add = (a, b) => a + b;
+let state = 1;
+
+export const createArea = (rows, cols) => {
+  const area = [];
+  const random = () => Math.floor(Math.random() * 2);
+
+  for (let i = 0; i < rows; i++) {
+    area[i] = [];
+
+    for (let j = 0; j < cols; j++) {
+      area[i][j] = random();
+    }
+  }
+
+  console.table(area);
+  return area;
+};
--- js/tools.test.js
+++ js/tools.test.js
@@ -1,10 +1,14 @@
-import { add } from "./tools.js";
-describe("sdfsd", () => {
-  test("sdfsdf", () => {
-    const a = 1;
-    const b = 1;
-    const result = add(a, b);
-    const expected = 2;
-    expect(result).toBe(expected);
+import { createArea } from "./tools.js";
+describe("Given a function createArea", () => {
+  describe("When we have two parameters for rows and columns", () => {
+    test("Then if we define 3 rows and 3 columns,it should appear nine random numbers", () => {
+      const rows = 3;
+      const cols = 3;
+      const expected = console.table(createArea(rows,cols);
+      const r = createArea(rows, cols);
+      expect(r).toBe(expected);
+    });
   });
 });
+
+createArea(3, 3);
