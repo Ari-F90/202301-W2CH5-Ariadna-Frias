555c2087bace1c15276c007fb5e013e6d2aabb0f
Ariadna Frias <ariadna.812@gmail.com>
2023-01-29 12:03:36 +0100

    Updating changes in test first function

--- js/createArea.test.js
+++ js/createArea.test.js
@@ -1,7 +1,7 @@
 import { createArea } from "./createArea.js";
 describe("Given a function createArea", () => {
   describe("When we have two parameters for rows and columns", () => {
-    test("Then if we define 3 rows and 3 columns,it should appear 9 random numbers", () => {
+    test("Then if we define 3 rows and 3 columns, each cell it should be a number", () => {
       const rows = 3;
       const cols = 3;
       const expected = "number";
