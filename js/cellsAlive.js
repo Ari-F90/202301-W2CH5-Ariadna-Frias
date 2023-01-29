4019718f96557e6a2b862b7147170237b75dd6b1
Ariadna Frias <ariadna.812@gmail.com>
2023-01-29 18:05:17 +0100

    Changes in second function loop

--- js/cellsAlive.js
+++ js/cellsAlive.js
@@ -5,7 +5,7 @@ export const countCellsAlive = (rows, cols, area) => {
   let cells = 0;
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      if (i !== rows - 1 || j !== cols - 1 || i !== -1 || j !== -1) {
+      if (i !== rows || j !== cols || i !== -1 || j !== -1) {
         if (area[i][j] === 1) {
           cells++;
         }
@@ -14,6 +14,5 @@ export const countCellsAlive = (rows, cols, area) => {
   }
 
   console.log(cells);
-
   return cells;
 };
